dd4857a2ed4d28f034e7c0a4024f08479f9c51ec
nagiarjun05 <nagiarjun05@gmail.com>
2022-11-19 13:06:23 +0530

    ContactUs and Succes page added

--- application.js
+++ application.js
@@ -8,11 +8,15 @@ const app=express();
 
 const adminRoutes=require('./routes/admin');
 const shopRoutes=require('./routes/shop');
+const contactUs=require('./routes/contacUs')
+const success=require('./routes/success')
 
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
+app.use(contactUs);
+app.use(success);
 
 app.use((req,res,next)=>{
     res.status(404).sendFile(path.join(__dirname,'views','404.html'))
---
+++ routes/contacUs.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const path=require('path');
+
+
+const router=express.Router();
+
+router.get('/contactUs',(req,res,next)=>{
+    res.sendFile(path.join(__dirname,'../','views','contactUs.html'));
+});
+
+// router.post('/contactUs',(req,res,next)=>{
+//     constole.log(req.body);
+//     res.redirect('/')
+// });
+
+module.exports=router;
